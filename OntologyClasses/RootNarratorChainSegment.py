467938fd44508773939ba554343486f6b37c18d1
A-Kamran <97387765+A-Kamran@users.noreply.github.com>
2024-07-23 14:18:57 +0500

    Delete OntologyClasses directory

--- OntologyClasses/RootNarratorChainSegment.py
+++
@@ -1,8 +0,0 @@
-import Owlready
-
-from OntologyClasses.NarratorChainSegment import NarratorChainSegment
-
-
-class RootNarratorChainSegment(NarratorChainSegment):
-    def createIndividual(self, onto: Owlready.owlready2.namespace.Ontology):
-        self.individual = onto.RootNarratorChainSegment(self.name)
